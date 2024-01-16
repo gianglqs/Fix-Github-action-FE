b63d9ecc3c0a61a2aeb41808be5201ad21dde7df
gianglqs <son.giang@phoenix-software.net>
2024-01-17 00:11:27 +0700

    setup api to call data

--- src/api/product.api.ts
+++ src/api/product.api.ts
@@ -1,12 +1,23 @@
 import HttpService from '@/helper/HttpService';
+import { GetServerSidePropsContext } from 'next';
+import { ResponseType } from 'axios';
 
 class ProductApi extends HttpService<any> {
    getInitDataFilter = () => {
-      return this.get<any>(`filters/booking`);
+      return this.get<any>(`filters/product`);
    };
 
    importDataProduct = (data: any) => {
-      return this.importData<any>('product/getData', data);
+      return this.importData<any>('product/importData', data);
+   };
+   getListProduct = <T = any>(
+      data = {} as Record<string, any>,
+      params = {} as Record<string, any>,
+      context: GetServerSidePropsContext = null as any,
+      responseType = 'default' as ResponseType
+   ) => {
+      this.saveToken(context);
+      return this.instance.post<T>(`product/getData`, data, { params, responseType });
    };
 }
 
