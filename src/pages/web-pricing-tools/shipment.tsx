25e67042b735b88a1e278c364eae7f3d69203f27
gianglqs <son.giang@phoenix-software.net>
2024-01-17 13:54:35 +0700

    adjust show modelCode

--- src/pages/web-pricing-tools/shipment.tsx
+++ src/pages/web-pricing-tools/shipment.tsx
@@ -166,7 +166,7 @@ export default function Shipment() {
          flex: 0.6,
          headerName: 'Models',
          renderCell(params) {
-            return <span>{params.row.model}</span>;
+            return <span>{params.row.productDimension.modelCode}</span>;
          },
       },
       {
