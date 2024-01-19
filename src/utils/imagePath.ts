f403818af4684fa261b1d0c86f1a16d3f3bd6840
gianglqs <son.giang@phoenix-software.net>
2024-01-19 23:32:06 +0700

    update function get image product

--- src/utils/imagePath.ts
+++ src/utils/imagePath.ts
@@ -1,3 +1,4 @@
 export const getProductImagePath = (imageName: string) => {
-   return process.env.NEXT_PUBLIC_BACKEND_URL + 'loadImage/product/' + imageName;
+   if (typeof imageName === 'string' && imageName.length !== 0)
+      return process.env.NEXT_PUBLIC_BACKEND_URL + 'loadImage/product/' + imageName;
 };
