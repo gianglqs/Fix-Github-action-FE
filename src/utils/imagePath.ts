a9f4817b0457183f077c6d1dc8f6c8bbcee7f4a0
gianglqs <son.giang@phoenix-software.net>
2024-01-21 12:20:30 +0700

    create new function to get Image from server

--- src/utils/imagePath.ts
+++ src/utils/imagePath.ts
@@ -1,4 +1,13 @@
 export const getProductImagePath = (imageName) => {
-   if (typeof imageName === 'string' && imageName.length !== 0)
+   if (checkNoneEmptyString(imageName))
       return process.env.NEXT_PUBLIC_BACKEND_URL + 'loadImage/product/' + imageName;
+};
+
+export const getPartImagePath = (imageName) => {
+   if (checkNoneEmptyString(imageName))
+      return process.env.NEXT_PUBLIC_BACKEND_URL + 'loadImage/part/' + imageName;
+};
+
+export const checkNoneEmptyString = (s) => {
+   return typeof s === 'string' && s.length !== 0;
 };
