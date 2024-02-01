00d9614ccde94136842739185ff153689e2d10ec
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-02-01 16:56:31 +0700

    add constant for getting currency's color

--- src/utils/constant.tsx
+++ src/utils/constant.tsx
@@ -1,11 +1,53 @@
-export const DATE_FORMAT = 'yyyy-MM-dd'
+export const DATE_FORMAT = 'yyyy-MM-dd';
 
 // string
-export const OK = 'OK'
-export const CANCEL = 'CANCEL'
-export const CLOSE = 'CLOSE'
-export const YES = 'YES'
-export const NO = 'NO'
-export const SUBMIT = 'SUBMIT'
-export const SAVE = 'SAVE'
-export const DELETE = 'DELETE'
+export const OK = 'OK';
+export const CANCEL = 'CANCEL';
+export const CLOSE = 'CLOSE';
+export const YES = 'YES';
+export const NO = 'NO';
+export const SUBMIT = 'SUBMIT';
+export const SAVE = 'SAVE';
+export const DELETE = 'DELETE';
+export const CURRENCY = {
+   EUR: '#1f78b4',
+   GBP: '#33a02c',
+   USD: '#e31a1c',
+   NLG: '#ff7f00',
+   DEM: '#6a3d9a',
+   FRF: '#b15928',
+   JPY: '#a6cee3',
+   ESP: '#b2df8a',
+   ITL: '#fb9a99',
+   BEF: '#fdbf6f',
+   DKK: '#ff7f00',
+   AUD: '#33a02c',
+   CAD: '#e5e548',
+   CHF: '#fdbf6f',
+   SEK: '#a6cee3',
+   NOK: '#1f78b4',
+   IEP: '#e31a1c',
+   FIM: '#ff7f00',
+   ZAR: '#b15928',
+   ATS: '#6a3d9a',
+   BRL: '#fdbf6f',
+   MXN: '#a6cee3',
+   HKD: '#33a02c',
+   SGD: '#e5e548',
+   CNY: '#1f78b4',
+   NZD: '#ff7f00',
+   INR: '#b15928',
+   AED: '#fdbf6f',
+   BHD: '#a6cee3',
+   SAR: '#e31a1c',
+   RUB: '#33a02c',
+   PLN: '#b2df8a',
+   THB: '#ff7f00',
+   MYR: '#b15928',
+   TWD: '#a6cee3',
+   CZK: '#fdbf6f',
+   RON: '#cab2d6',
+   KRW: '#cdcd88',
+   LKR: '#b2df8a',
+   VND: '#fdbf6f',
+};
