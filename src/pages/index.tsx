e43136a6b3a244c39af0f1c2fcc0e6a8de4537b3
gianglqs <son.giang@phoenix-software.net>
2024-02-19 17:32:01 +0700

    auto redirect to /login when access in /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import LoginPage from './login';
 import { checkTokenBeforeLoadPageLogin } from '@/utils/checkTokenBeforeLoadPage';
 import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
    return await checkTokenBeforeLoadPageLogin(context);
 }
 
 function IndexPage() {
-   return <LoginPage />;
+   const router = useRouter();
+   useEffect(() => {
+      router.push('/login');
+   });
 }
 
 export default IndexPage;
