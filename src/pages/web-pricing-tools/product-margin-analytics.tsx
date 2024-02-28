6ee7095e6c93971f1e783afb22d70d97027f283c
gianglqs <son.giang@phoenix-software.net>
2024-02-28 17:41:24 +0700

    add label latest updated time in screen outlier

--- src/pages/web-pricing-tools/product-margin-analytics.tsx
+++ src/pages/web-pricing-tools/product-margin-analytics.tsx
@@ -6,7 +6,7 @@ import { outlierStore, commonStore } from '@/store/reducers';
 
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 import { AppAutocomplete, AppDateField, AppLayout, DataTablePagination } from '@/components';
 
@@ -41,6 +41,7 @@ ChartJS.register(
    ChartAnnotation
 );
 import { rowColor } from '@/theme/colorRow';
+import moment from 'moment-timezone';
 
 import { checkTokenBeforeLoadPage } from '@/utils/checkTokenBeforeLoadPage';
 import { GetServerSidePropsContext } from 'next';
@@ -62,6 +63,10 @@ export default function Outlier() {
    const [dataFilter, setDataFilter] = useState(cacheDataFilter);
    const [loading, setLoading] = useState(false);
    const [hasSetDataFilter, setHasSetDataFilter] = useState(false);
+   const serverTimeZone = useSelector(outlierStore.selectServerTimeZone);
+   const serverLatestUpdatedTime = useSelector(outlierStore.selectLatestUpdatedTime);
+
+   const [clientLatestUpdatedTime, setClientLatestUpdatedTime] = useState('');
 
    const handleChangeDataFilter = (option, field) => {
       setDataFilter((prev) =>
@@ -463,6 +468,22 @@ export default function Outlier() {
       setDataFilter(defaultValueFilterOrder);
    };
 
+   // show latest updated time
+   const convertServerTimeToClientTimeZone = () => {
+      if (serverLatestUpdatedTime && serverTimeZone) {
+         const clientTimeZone = moment.tz.guess();
+         const convertedTime = moment
+            .tz(serverLatestUpdatedTime, serverTimeZone)
+            .tz(clientTimeZone);
+         setClientLatestUpdatedTime(convertedTime.format('HH:mm:ss YYYY-MM-DD'));
+         console.log('Converted Time:', convertedTime.format());
+      }
+   };
+
+   useEffect(() => {
+      convertServerTimeToClientTimeZone();
+   }, [serverLatestUpdatedTime, serverTimeZone]);
+
    return (
       <>
          <AppLayout entity="outlier">
@@ -636,6 +657,11 @@ export default function Outlier() {
                   >
                      Clear
                   </Button>
+               </Grid>
+               <Grid sx={{ display: 'flex', justifyContent: 'end' }} xs={12}>
+                  <Typography sx={{ marginRight: '20px' }}>
+                     Latest updated at {clientLatestUpdatedTime}
+                  </Typography>
                </Grid>
             </Grid>
             <Grid
