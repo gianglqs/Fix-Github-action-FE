0a718f843b4f4597bf82f3721e7233f927ac9a50
gianglqs <son.giang@phoenix-software.net>
2024-03-04 16:29:04 +0700

    perf(load image): adjust load Product image

--- src/components/App/Image/ProductImage.tsx
+++ src/components/App/Image/ProductImage.tsx
@@ -1,4 +1,5 @@
 const defaultProductImage = require('@/public/images/defaultProductImage.png');
+import { PRODUCT_IMAGE_FOLDER } from '@/Path/backend';
 import { checkNoneEmptyString } from '@/utils/imagePath';
 import { useEffect, useState } from 'react';
 
@@ -7,24 +8,24 @@ export function ProductImage(props) {
    const [correctImageUrl, setCorrectImageUrl] = useState('');
    useEffect(() => {
       if (isImageChoose) {
-         console.log('chooese image');
          setCorrectImageUrl(imageUrl);
       } else if (checkNoneEmptyString(imageUrl)) {
-         fetch(process.env.NEXT_PUBLIC_BACKEND_URL + imageUrl).then((res) => {
-            console.log('ression', res);
-            if (res.ok) {
-               setCorrectImageUrl(process.env.NEXT_PUBLIC_BACKEND_URL + imageUrl);
-            } else {
-               setCorrectImageUrl(defaultProductImage);
+         fetch(process.env.NEXT_PUBLIC_BACKEND_URL + PRODUCT_IMAGE_FOLDER + imageUrl).then(
+            (res) => {
+               if (res.ok) {
+                  setCorrectImageUrl(
+                     process.env.NEXT_PUBLIC_BACKEND_URL + PRODUCT_IMAGE_FOLDER + imageUrl
+                  );
+               }
+               if (res.status === 404) {
+                  setCorrectImageUrl(defaultProductImage);
+               }
             }
-         });
-      } else {
+         );
+      } else if (imageUrl !== undefined) {
          setCorrectImageUrl(defaultProductImage);
       }
    }, [imageUrl]);
-
-   console.log(imageUrl);
-   console.log(correctImageUrl);
 
    return <img src={correctImageUrl} style={style} onClick={onClick} />;
 }
--- src/components/Dialog/Module/ProductManangerDialog/ProductDetailDialog.tsx
+++ src/components/Dialog/Module/ProductManangerDialog/ProductDetailDialog.tsx
@@ -199,7 +199,7 @@ const ProductDetailDialog: React.FC<any> = (props) => {
 
    const handleClose = () => {
       onClose();
-      setTimeout(resetData, 1000);
+      setTimeout(resetData, 500);
    };
 
    // reset when CLOSE dialog
