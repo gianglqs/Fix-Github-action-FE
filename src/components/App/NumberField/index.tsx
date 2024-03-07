2c97f394d86050ff5c6ae7d584c23be0311f5b4b
gianglqs <son.giang@phoenix-software.net>
2024-03-07 14:12:52 +0700

    fix: only allow enter number

--- src/components/App/NumberField/index.tsx
+++ src/components/App/NumberField/index.tsx
@@ -1,31 +1,32 @@
-import { forwardRef } from "react"
+import { forwardRef } from 'react';
 
-import { AppTextField } from "@/components"
+import { AppTextField } from '@/components';
 
-import type { AppNumberFieldProps } from "./type"
-import { NumberFormatBase } from 'react-number-format'
+import type { AppNumberFieldProps } from './type';
+import { NumericFormat } from 'react-number-format';
+import { truncate } from 'fs';
 
-const AppNumberField: React.FC<AppNumberFieldProps> = forwardRef(
-  (props, ref) => {
-    const { onChange, ...numberFieldProps } = props
+const AppNumberField: React.FC<AppNumberFieldProps> = forwardRef((props, ref) => {
+   const { onChange, ...numberFieldProps } = props;
 
-    return (
-      <NumberFormatBase
-        {...(numberFieldProps as any)}
-        customInput={AppTextField}
-        onValueChange={onChange}
-        ref={ref as any}
+   return (
+      <NumericFormat
+         {...(numberFieldProps as any)}
+         customInput={AppTextField}
+         onValueChange={onChange}
+         ref={ref as any}
+         allowNegative={true}
+         format="#### #### #### ####"
+         fixedDecimalScale={true}
       />
-    )
-  }
-)
+   );
+});
 
 AppNumberField.defaultProps = {
-  thousandSeparator: " ",
-  allowNegative: false,
-  decimalScale: 2,
-  fixedDecimalScale: 2,
-}
+   thousandSeparator: ' ',
+   decimalScale: 0,
+   fixedDecimalScale: true,
+};
 
-export * from "./type"
-export { AppNumberField }
+export * from './type';
+export { AppNumberField };
