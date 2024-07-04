016e7f3062b6a5645652a15fc0c9fbc40d883e2d
gianglqs <son.giang@phoenix-software.net>
2024-07-04 10:42:10 +0700

    feat: show message when auto redirect page

--- src/pages/404.ts
+++ src/pages/404.ts
@@ -1,13 +1,18 @@
 import { PATH_ADMIN_USER, PATH_BOOKING } from '@/Path/frontend';
+import { commonStore } from '@/store/reducers';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 
 export default function handleRedirectToBasePage() {
    const router = useRouter();
+   const dispatch = useDispatch();
+   const { t } = useTranslation();
 
    useEffect(() => {
       const currentURL = window.location.pathname;
-
+      dispatch(commonStore.actions.setNotificationMessage(t('commonErrorMessage.pageNotFound')));
       if (currentURL.includes('admin')) {
          router.push(PATH_ADMIN_USER);
       } else {
