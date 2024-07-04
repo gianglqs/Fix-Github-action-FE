8f65d7385a64251800971eb5ea958c107c28ac85
gianglqs <son.giang@phoenix-software.net>
2024-07-04 09:32:02 +0700

    adjust get data of column discount and re-range table

--- src/pages/web-pricing-tools/quotation-margin.tsx
+++ src/pages/web-pricing-tools/quotation-margin.tsx
@@ -121,10 +121,7 @@ export default function MarginAnalysis() {
          const marginAnalystData = data?.MarginAnalystData;
 
          marginAnalystData.forEach((margin) => {
-            margin.discountPercentage = (
-               1 -
-               margin.dealerNet / (margin.listPrice === 0 ? 1 : margin.listPrice)
-            ).toLocaleString();
+            margin.discount = formatNumberPercentage(margin.discount * 100);
             margin.listPrice = margin.listPrice.toLocaleString();
             margin.manufacturingCost = margin.manufacturingCost.toLocaleString();
             margin.dealerNet = margin.dealerNet.toLocaleString();
@@ -314,6 +311,16 @@ export default function MarginAnalysis() {
       },
 
       {
+         field: 'discount',
+         flex: 0.4,
+         minWidth: 130,
+         headerName: `${t('table.discount')} (%)`,
+         headerAlign: 'right',
+         align: 'right',
+         cellClassName: 'highlight-cell',
+      },
+
+      {
          field: 'dealerNet',
          flex: 0.4,
          minWidth: 130,
@@ -323,15 +330,6 @@ export default function MarginAnalysis() {
          cellClassName: 'highlight-cell',
       },
 
-      {
-         field: 'discountPercentage',
-         flex: 0.4,
-         minWidth: 130,
-         headerName: `${t('table.discount')} (%)`,
-         headerAlign: 'right',
-         align: 'right',
-         cellClassName: 'highlight-cell',
-      },
       {
          field: 'manufacturingCost',
          flex: 0.7,
