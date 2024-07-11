9755e1ca0ad71b893590e6eaf9bf96658904937b
khuongdinh296 <khuong.dinh@phoenix-software.net>
2024-07-11 18:16:27 +0700

    fix: competitor name not display in update dialog

--- src/pages/web-pricing-tools/admin/competitor-colors.tsx
+++ src/pages/web-pricing-tools/admin/competitor-colors.tsx
@@ -159,7 +159,6 @@ export default function competitorColors() {
          // Get init data
 
          const { data } = await competitorColorApi.getCompetitorColorById({ id });
-
          // Open form
          setUpdateColorState({
             open: true,
