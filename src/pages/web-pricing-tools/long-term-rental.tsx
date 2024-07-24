797ecbd9073d35c0663df619441e6d2806463654
khuongdinh296 <khuong.dinh@phoenix-software.net>
2024-07-24 17:40:20 +0700

    chores: remove unused code

--- src/pages/web-pricing-tools/long-term-rental.tsx
+++ src/pages/web-pricing-tools/long-term-rental.tsx
@@ -826,13 +826,6 @@ const GeneralInput = () => {
       localStorage.setItem('generalFilterLongTermRental', JSON.stringify(newSelectedFilter));
 
       fetchSelectOptions(newSelectedFilter);
-
-      /* //so we need to clear Model selection here
-      if (field == 'series') {
-         dispatch(
-            longTermRentalStore.actions.setSelectedFilters({ ...selectedFilter, modelCode: null })
-         );
-      }*/
    };
    const handleCalculate = () => {
       if (!isAbleToCalculate) {
