5f9ee5fafa3680fb434ddc0199a8067dd4be881e
gianglqs <son.giang@phoenix-software.net>
2024-07-30 08:42:39 +0700

    create common component UploadFileDropZone

---
+++ src/components/App/UploadFileDropZone/index.tsx
@@ -0,0 +1,60 @@
+import { commonStore } from '@/store/reducers';
+import { Button } from '@mui/material';
+import _ from 'lodash';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+
+export function UploadFileDropZone(props) {
+   const { t } = useTranslation();
+   const onDrop = useCallback((acceptedFiles) => {
+      acceptedFiles.forEach((file) => {
+         const reader = new FileReader();
+
+         reader.onabort = () => console.log('file reading was aborted');
+         reader.onerror = () => console.log('file reading has failed');
+         reader.onload = () => {
+            // Do whatever you want with the file contents
+            props.setFileName && props.setFileName(file.name);
+         };
+         reader.readAsArrayBuffer(file);
+         props.handleUploadFile(file);
+      });
+   }, []);
+
+   const { getRootProps, getInputProps, open, fileRejections } = useDropzone({
+      noClick: true,
+      onDrop,
+      maxSize: 50777216,
+      maxFiles: 1,
+      accept: {
+         'excel/xlsx': ['.xlsx', '.xlsb'],
+      },
+   });
+   const dispatch = useDispatch();
+   const isFileInvalid = fileRejections.length > 0 ? true : false;
+   if (isFileInvalid) {
+      const errors = fileRejections[0].errors;
+      dispatch(
+         commonStore.actions.setErrorMessage(
+            `${errors[0].message} ${_.isNil(errors[1]) ? '' : `or ${errors[1].message}`}`
+         )
+      );
+      fileRejections.splice(0, 1);
+   }
+
+   return (
+      <div {...getRootProps()}>
+         <input {...getInputProps()} />
+         <Button
+            type="button"
+            onClick={open}
+            variant="contained"
+            sx={{ width: '100%', height: 24, ...props.sx }}
+         >
+            {t(props.buttonName)}
+         </Button>
+      </div>
+   );
+}
